58a04a9cf91585fb7090ffc232fb5be07c7314d7
Brandon Zucker <bwzuck12@gmail.com>
2018-11-29 14:11:57 -0700

    added a few fixes to the JS file, took clock out of military time and added a 0 in front of single digit values

--- app.js
+++ app.js
@@ -2,16 +2,24 @@ function digiClock() {
 	const fullDate = new Date();
 	// console.log(fullDate);
 	let hours = fullDate.getHours();
-	const mins = fullDate.getMinutes();
-	const secs = fullDate.getSeconds();
+	let mins = fullDate.getMinutes();
+	let secs = fullDate.getSeconds();
 	// console.log(fullDate.getHours());
 	// console.log(fullDate.getMinutes());
 	// console.log(fullDate.getSeconds());
 
 	if (hours > 12) {
 		hours = hours - 12;
-	} else {
-		return
+	} 
+
+	if (hours < 10) {
+		hours = "0" + hours;
+	}
+	if (mins < 10) {
+		mins = "0" + mins;
+	}
+	if (secs < 10) {
+		secs = "0" + secs;
 	}
 
 	// console.log(document.getElementById('hour'));
